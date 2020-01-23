05827bf763e0fec665506b8d842b824959b7c126
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-01-23 18:38:50 -0500

    Completed Task 5 and Halfway through 6.

--- index.js
+++ index.js
@@ -169,7 +169,23 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(Par){
+    super(Par);
+    this.previousBackground = Par.previousBackground;
+    this. className = Par.className;
+    this.favSubjects = Par.favSubjects;
+  }
+  listSubjects(){
+    return `Love ${this.favSubjects}`;
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 
 }
 
@@ -186,7 +202,19 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
+  constructor(Par){
+    super(Par);
+    this. gradClassName = Par.gradClassName;
+    this.favInstructor = Par.favInstructor; 
+  }
+  standUp(slackChannel){
+    return `${name} announces to ${slackChannel}, @channel standy times!`;
+  }
+  debugsCode(studen, subject){
+    return `${name} debugs ${this.name}'s code on ${subject}`;
+
+  }
 
 }
 
